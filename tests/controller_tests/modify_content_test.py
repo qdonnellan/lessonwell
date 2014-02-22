ea7bb7c2e23f396ef7808c3a9406e76aaf04c328
qdonnellan <qdonnellan@gmail.com>
2014-02-22 12:29:42 -0600

    added the modify content tests

--- tests/controller_tests/modify_content_test.py
+++ tests/controller_tests/modify_content_test.py
@@ -1,6 +1,7 @@
 from tests.main_test_handler import TestBase
-
 from controllers.modify_content import new_content, edit_content
+
+from models.content import Content
 
 class ModifyContentTest(TestBase):
     '''
@@ -11,4 +12,33 @@ class ModifyContentTest(TestBase):
         '''
         test that the new content controller is working correctly
         '''
-        pass
+        local_user = self.create_and_return_local_user()
+        new_content(
+            title = 'Foo',
+            body = 'A lesson on bar',
+            parentKEY = local_user.key,
+            contentType = 'course',
+            privacy = 'public',
+            listed = None
+            )
+        self.assertEqual(Content.query().count(),1)
+
+    def test_edit_content_controller(self):
+        '''
+        test the edit content controller is working fine
+        '''
+        local_user = self.create_and_return_local_user()
+        content_id = new_content(
+            title = 'Foo',
+            body = 'A lesson on bar',
+            parentKEY = local_user.key,
+            contentType = 'course',
+            privacy = 'public',
+            listed = None
+            )
+        self.assertEqual(Content.query().count(),1)
+        edit_content(contentID = content_id, parentKEY = local_user.key, title="NewFoo")
+        
+
+
+
