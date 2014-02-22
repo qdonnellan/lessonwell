923b575926358ec9e3838294c9a500a2c90fae5e
qdonnellan <qdonnellan@gmail.com>
2014-02-22 12:29:19 -0600

    added some useful methods to the main test handler

--- tests/main_test_handler.py
+++ tests/main_test_handler.py
@@ -4,6 +4,10 @@ import webtest
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 
+from controllers.modify_user import new_user
+from controllers.fetch_user import get_user_by_username
+from controllers.modify_content import new_content
+
 import main
 
 class TestBase(unittest.TestCase):
@@ -22,11 +26,29 @@ class TestBase(unittest.TestCase):
     def tearDown(self):
         self.testbed.deactivate()
 
-    def createUser(self, email_address, admin=False, user_id = '123'):
+    def create_google_user(self, email_address, admin=False, user_id = '123'):
         self.testbed.setup_env(
             USER_EMAIL = email_address,
             USER_ID = user_id,
             USER_IS_ADMIN = '1' if admin else '0',
             overwrite = True
         )
+
+    def create_and_return_local_user(self, username='testuser123'):
+        '''
+        create a user with a default email address and formal name, return that object
+
+        a helper function for tests that require a local (app) user to operate
+        '''
+        new_user(username = username, email='test@example.com', formalName ='James Smith', googleID = '123')
+        return get_user_by_username(username)
+
+    def create_sample_course(self, title='foo', body='bar', user = None):
+        if not user:
+            user = self.create_and_return_local_user()
+        content_id = new_content(title=title, body=body, parentKEY=user.key, contentType='course', privacy=None, listed=None)
+        return content_id
+
+
+
 
