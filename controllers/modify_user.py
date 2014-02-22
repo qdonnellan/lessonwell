dc85911f1e34c95b5ebbe2ad5a937cbccce4b856
qdonnellan <qdonnellan@gmail.com>
2014-02-22 11:51:46 -0600

    removed reference to localUser object, now get user just gets user by username

--- controllers/modify_user.py
+++ controllers/modify_user.py
@@ -1,5 +1,5 @@
 from models.user import User
-from fetch_user import get_user
+from fetch_user import get_user_by_username
 from validation import validate_username
 
 def new_user(username, formalName, email, googleID, stripeID=None):  
@@ -19,23 +19,19 @@ def new_user(username, formalName, email, googleID, stripeID=None):
     userObject.put()
     return userObject
 
-def edit_user(formalName, bio, pic, tagline, localUser):
+def edit_user(username, formalName=None, bio=None, pic=None, tagline=None, stripeID = None):
     '''
     edit an existing user by their username, return the user object
     '''
-    user = get_user(localUser.username)
-    user.populate(
-        formalName = formalName,
-        bio = bio,
-        pic = pic,
-        tagline = tagline,      
-        )
-    user.put()
-    return user
+    user = get_user_by_username(username)
+    if user:
+        if formalName: user.formalName = formalName
+        if bio: user.bio = bio
+        if pic: user.pic = pic
+        if tagline: user.tagline = tagline
+        if stripeID: user.stripeID = stripeID
+        user.put()
+        return user
+    else:
+        return False
 
-def assign_stripe_id(user, stripeID):
-    '''
-    assign a passed stripeID to the user
-    '''
-    user.stripeID = stripeID
-    user.put()
