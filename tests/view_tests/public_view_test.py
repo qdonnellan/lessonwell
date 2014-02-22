fac320731cb12ce62eda3784eb64aaf8959b38dd
qdonnellan <qdonnellan@gmail.com>
2014-02-22 17:21:04 -0600

    added some more test to the public view pages

--- tests/view_tests/public_view_test.py
+++ tests/view_tests/public_view_test.py
@@ -12,4 +12,37 @@ class PublicViewTest(TestBase):
         response = self.testapp.get('/')
         self.assertEqual(response.status_int, 200)
 
+    def test_public_about_page_view(self):
+        '''
+        test views of '/about' and 'about/?doc=terms', 'about/?doc=privacy', etc.
+        '''
+        for page in ['/about', '/about/?doc=terms', '/about/?doc=privacy']:
+            response = self.testapp.get('/about')
+            self.assertEqual(response.status_int, 200)
+            self.assertIn('About', response.body)
+
+    def test_public_sign_up_page_view_not_authenticated(self):
+        '''
+        test views of '/sign_up'
+        '''
+        response = self.testapp.get('/sign_up')
+        self.assertEqual(response.status_int, 302)
+
+    def test_public_sign_up_page_view_with_google_authentication(self):
+        '''
+        test views of '/sign_up' with a user authenticated to google
+        '''
+        self.create_google_user()
+        response = self.testapp.get('/sign_up')
+        self.assertEqual(response.status_int, 200)
+
+    def test_public_link_google_account_page(self):
+        '''
+        test views of '/link'
+        '''
+        response = self.testapp.get('/link')
+        self.assertEqual(response.status_int, 200)
+
+
+
 
