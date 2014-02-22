378ebefeee9ee288a48870fca7c657df92a82737
qdonnellan <qdonnellan@gmail.com>
2014-02-22 17:21:34 -0600

    slowly adding the new views

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import webapp2
 from views.public.front_page import FrontPage
+from views.public.about_page import AboutPage
+from views.public.sign_up_page import SignUpPage
+from views.public.link_google_account_page import LinkGoogleAccountPage
 
 app = webapp2.WSGIApplication([
-  ('.*', FrontPage),
-  ],debug=False)
+    ('/about', AboutPage),
+    ('/sign_up', SignUpPage),
+    ('/link', LinkGoogleAccountPage), 
+    ('.*', FrontPage),
+    ],debug=False)
