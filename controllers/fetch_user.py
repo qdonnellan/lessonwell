1b885f8d48d686f582d61697a6c426d5ee9bfe8e
qdonnellan <qdonnellan@gmail.com>
2014-02-23 11:20:56 -0600

    added method for fetching user by user id

--- controllers/fetch_user.py
+++ controllers/fetch_user.py
@@ -1,4 +1,5 @@
 from models.user import User
+from google.appengine.ext import ndb
 
 def get_user_by_username(username):
     '''
@@ -12,4 +13,11 @@ def get_user_by_google_id(googleID):
     return the user object given their googleID
     '''
     user = User.query(User.googleID == str(googleID)).get()
+    return user
+
+def get_user(userID):
+    '''
+    return the user object given their userID
+    '''
+    user = ndb.Key(User, int(userID)).get()
     return user
