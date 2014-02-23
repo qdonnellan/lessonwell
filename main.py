de06f029591c9d259c41a4940f170f346440e008
qdonnellan <qdonnellan@gmail.com>
2014-02-23 11:21:25 -0600

    added /api/content routes

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from views.public.front_page import FrontPage
 from views.public.about_page import AboutPage
 from views.public.sign_up_page import SignUpPage
 from views.public.link_google_account_page import LinkGoogleAccountPage
+from api.request_handlers.content_api import ContentAPI
 
 app = webapp2.WSGIApplication([
+    ('/api/content/(\w+)', ContentAPI), 
+    ('/api/content/(\w+)/(\w+)', ContentAPI), 
+    ('/api/content/(\w+)/(\w+)/(\w+)', ContentAPI),
+    ('/api/content/(\w+)/(\w+)/(\w+)/(\w+)', ContentAPI),
     ('/about', AboutPage),
     ('/sign_up', SignUpPage),
     ('/link', LinkGoogleAccountPage), 
