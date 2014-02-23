9176b5c5ace8635ffe0a1a6d644b8b03561cbff5
qdonnellan <qdonnellan@gmail.com>
2014-02-23 11:24:17 -0600

    a helper function that returns a boolean for privacy status of course

---
+++ controllers/check_privacy.py
@@ -0,0 +1,8 @@
+def check_privacy(course):
+    '''
+    return true if course is private and false otherwise
+    '''
+    if course.privacy == 'private':
+        return True 
+    else:
+        return False
