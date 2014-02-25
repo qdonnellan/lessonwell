8ca951292c6bf9d76e08858c39ab7615f4e273b2
qdonnellan <qdonnellan@gmail.com>
2014-02-24 18:53:50 -0600

    passing none to check privacy should raise an exception

--- controllers/check_privacy.py
+++ controllers/check_privacy.py
@@ -2,7 +2,10 @@ def check_privacy(course):
     '''
     return true if course is private and false otherwise
     '''
-    if course.privacy == 'private':
-        return True 
+    if not course:
+        raise Exception('cannot check the privacy of NoneType course')
     else:
-        return False
+        if course.privacy == 'private':
+            return True 
+        else:
+            return False
