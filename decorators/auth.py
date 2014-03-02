3e2832f3130353d5472b930e6e2705ed0ed16d21
qdonnellan <qdonnellan@gmail.com>
2014-03-02 17:53:34 -0600

    renamed an old decorator, added a new one

--- decorators/auth.py
+++ decorators/auth.py
@@ -2,8 +2,9 @@ from google.appengine.api import users
 from controllers.fetch_content_by_id import get_course
 from controllers.check_privacy import check_privacy
 from controllers.fetch_approval import get_approval_status_for_google_id
+from controllers.fetch_user import get_user_by_google_id
 
-def google_user_required(decorated_function):
+def link_if_no_google_user(decorated_function):
     '''
     if the current_user is not authenticated to this app, redirect to the link page
     '''
@@ -14,6 +15,34 @@ def google_user_required(decorated_function):
         else:
             decorated_function(self, *kw, **kwargs)
     return check_current_user
+
+def abort_if_no_google_user(decorated_function):
+    '''
+    if the current_user is not authenticated to this app, abort with a 401 error
+    '''
+    def check_current_user(self, *kw, **kwargs):
+        current_user = users.get_current_user()
+        if not current_user:
+            self.abort(401)
+        else:
+            decorated_function(self, *kw, **kwargs)
+    return check_current_user
+
+def local_user_required(decorated_function):
+    '''
+    if no local user is present, the request is aborted
+    '''
+    def check_for_local_user(self, *kw, **kwargs):
+        try:
+            current_user = users.get_current_user()
+            user = get_user_by_google_id(current_user.user_id())
+            if not user:
+                raise Exception
+        except:
+            self.abort(401)
+        else:
+            decorated_function(self, *kw, **kwargs)
+    return check_for_local_user
 
 def check_approval(decorated_function):
     '''
