29572f47910b837a6af95f5e9b568bb6ec583ae3
qdonnellan <qdonnellan@gmail.com>
2014-03-02 17:53:58 -0600

    added a couple more views and their appropriate routes

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from views.public.front_page import FrontPage
 from views.public.about_page import AboutPage
 from views.public.sign_up_page import SignUpPage
 from views.public.link_google_account_page import LinkGoogleAccountPage
+from views.teacher.success_page import SuccessPage
 from api.request_handlers.content_api import ContentAPI
+from api.request_handlers.validate_username_api import ValidateUsernameAPI
 
 app = webapp2.WSGIApplication([
     ('/api/content/(\w+)', ContentAPI), 
     ('/api/content/(\w+)/(\w+)', ContentAPI), 
     ('/api/content/(\w+)/(\w+)/(\w+)', ContentAPI),
     ('/api/content/(\w+)/(\w+)/(\w+)/(\w+)', ContentAPI),
+    ('/api/validate_username/(\w+)', ValidateUsernameAPI),
+    ('/success', SuccessPage),
     ('/about', AboutPage),
     ('/sign_up', SignUpPage),
     ('/link', LinkGoogleAccountPage), 
