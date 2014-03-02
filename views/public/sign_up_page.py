041c3e626703b7d050e17c6acb9cdc481e65fb2e
qdonnellan <qdonnellan@gmail.com>
2014-03-02 17:57:58 -0600

    the sign up page now has a post method and no longer calls stripe for every get request

--- views/public/sign_up_page.py
+++ views/public/sign_up_page.py
@@ -1,24 +1,54 @@
 from views.main_view_handler import ViewHandler
-from decorators.auth import google_user_required
-from stripe_keys import api_key
+from decorators.auth import link_if_no_google_user, abort_if_no_google_user
+from controllers.validation import validate_username
+from controllers.modify_user import new_user
+from controllers.stripe_controllers.new_customer import new_customer
+from google.appengine.api import users
 import datetime
-import stripe
 
 class SignUpPage(ViewHandler):
     '''
     the request handler for the sign_up page
     '''
     
-    @google_user_required
+    @link_if_no_google_user
     def get(self):
         '''
         render the sign_up page
         '''
-        stripe_plan = stripe.Plan.retrieve('standard', api_key = api_key)
-        trialEnd = datetime.datetime.now() + datetime.timedelta(days=stripe_plan.trial_period_days)
+        trialEnd = datetime.datetime.now() + datetime.timedelta(days=30)
         trialEnd = trialEnd.strftime("%A, %B %d").replace(' 0', ' ')
-        planAmount = stripe_plan.amount/100
-        planAmount = "%1.2f"  % planAmount
+        planAmount = "10.00"
         self.render('sign_up.html',
             trialEnd = trialEnd,
             planAmount = planAmount)
+
+    @abort_if_no_google_user
+    def post(self):
+        '''
+        handle a post request, which may or may not contain errors
+        '''
+        username = self.request.get('username')
+        stripeToken = self.request.get('stripeToken')
+        formalName = self.request.get('formalName')
+        email = users.get_current_user().email()
+        try: 
+            validate_username(username)
+            if not stripeToken or stripeToken == '': 
+                raise NameError('Invalid Payment Information')
+            user = new_user(
+                username = username, 
+                formalName = formalName, 
+                email = email,
+                googleID = users.get_current_user().user_id()
+                )
+            if not user:
+                raise NameError('There was a problem creating your user account, please try again')
+            new_customer(stripeToken, email, user)
+
+        except Exception as e:
+            self.write(str(e))
+            self.redirect('/sign_up?error=%s' % e)
+        else:
+            self.redirect('/success')
+
