1fee492bfbba5562f11837e6ec389d0946108bb5
qdonnellan <qdonnellan@gmail.com>
2014-03-03 09:43:07 -0600

    added sandbox view to the router

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import webapp2
 from views.public.front_page import FrontPage
 from views.public.about_page import AboutPage
 from views.public.sign_up_page import SignUpPage
+from views.public.sandbox import Sandbox
 from views.public.link_google_account_page import LinkGoogleAccountPage
 from views.teacher.success_page import SuccessPage
 from api.request_handlers.content_api import ContentAPI
@@ -14,6 +15,7 @@ app = webapp2.WSGIApplication([
     ('/api/content/(\w+)/(\w+)/(\w+)/(\w+)', ContentAPI),
     ('/api/validate_username/(\w+)', ValidateUsernameAPI),
     ('/success', SuccessPage),
+    ('/sandbox', Sandbox),
     ('/about', AboutPage),
     ('/sign_up', SignUpPage),
     ('/link', LinkGoogleAccountPage), 
