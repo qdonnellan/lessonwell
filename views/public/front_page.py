0575cc7cba6c77c0f93b59561ef5fdfb17c2ef7c
qdonnellan <qdonnellan@gmail.com>
2014-03-03 09:47:08 -0600

    renamed home page

--- views/public/front_page.py
+++ views/public/front_page.py
@@ -2,4 +2,4 @@ from views.main_view_handler import ViewHandler
 
 class FrontPage(ViewHandler):
     def get(self):
-        self.render('home.html', homeActive = 'active')
+        self.render('front.html', homeActive = 'active')
