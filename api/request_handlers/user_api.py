76c7dfccd95b56c664e64996e436b461fdd6abc2
qdonnellan <qdonnellan@gmail.com>
2014-03-06 06:34:03 -0600

    added the user api

---
+++ api/request_handlers/user_api.py
@@ -0,0 +1,23 @@
+from main_api_handler import APIHandler
+from controllers.fetch_user import get_user
+from api.api_controllers.user_to_dict import user_to_dict
+
+class UserAPI(APIHandler):
+    '''
+    handle all get/post requests for the user api
+    '''
+
+    def get(self, userID):
+        ''' 
+        handle the get request userID; return none if userID does not exist
+        '''
+        try:
+            user = get_user(userID)
+            if not user:
+                raise Exception('that user does not exist')
+            data = user_to_dict(user)
+
+        except Exception as e:
+            data = {'error' : str(e)}
+
+        self.write_json(data)
