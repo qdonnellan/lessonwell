9104a82b613703697d284fc33295c23a3dabcc55
qdonnellan <qdonnellan@gmail.com>
2014-03-06 06:33:55 -0600

    added user to dict controller

---
+++ api/api_controllers/user_to_dict.py
@@ -0,0 +1,17 @@
+def user_to_dict(user):
+    '''
+    return a dictionary object based on the user model passed
+    '''
+
+    data = {}
+
+    data['username'] = user.username
+    data['username_lower'] = user.username_lower
+    data['id'] = user.key.id()
+    data['googleID'] = user.googleID
+    data['formal_name'] = user.formalName
+    data['email'] = user.email
+    data['bio'] = user.bio
+    data['stripeID'] = user.stripeID
+
+    return data
