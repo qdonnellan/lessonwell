a68388623885e34766a7219bfbb59388aa11aa70
qdonnellan <qdonnellan@gmail.com>
2014-03-16 17:55:25 -0500

    moved the formatting function to a separate file

--- public/js/sandbox.js
+++ public/js/sandbox.js
@@ -1,31 +1,10 @@
 function ViewModel() {
     var self = this;
-    self.sandboxInput = ko.observable('')
-
+    self.sandboxInput = ko.observable('');
     self.sandboxOutput = ko.computed(function() {
         var input_str = self.sandboxInput();
-        var links = input_str.match(/\bhttps?[:][\/][\/]\S*\b/g);
-        if (links) {
-            for (var i = 0; i < links.length; i++) {
-                var link = links[i];
-                input_str = input_str.replace(link, self.formatLink(link));
-            }
-        }
-        //remove the link placeholder
-        input_str = input_str.replace(new RegExp('::LINK::PLACEHOLDER::', 'g'), 'http');
-        return markdown.toHTML(input_str);
+        return formatInput(input_str);
     });
-
-    self.formatLink = function(link) {
-        //add a link placeholder so that we don't replace the same link more than once
-        link = link.replace('http', '::LINK::PLACEHOLDER::');
-        if (link.match(/\S*[.]png|jpg|bmp|jpeg|tiff|tif|gif|jp2\b/)) {
-            return "![" + link + "](" + link + ")";
-        }
-        else {
-            return "[" + link + "](" + link + ")";
-        }  
-    };
 
     self.currentGif = ko.observable('/img/instructional_gifs/headers.gif')
 
