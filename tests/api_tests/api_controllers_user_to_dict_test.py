ca0130a2aac2f106fce7acaae67c2c340847c424
qdonnellan <qdonnellan@gmail.com>
2014-03-16 09:15:09 -0500

    refactored the DOCSTRING, changed the aesthetics of a function call

--- tests/api_tests/api_controllers_user_to_dict_test.py
+++ tests/api_tests/api_controllers_user_to_dict_test.py
@@ -4,14 +4,19 @@ from api.api_controllers.user_to_dict import user_to_dict
 
 class UserToDictTest(TestBase):
     '''
-    test the implementation of the user model to dictionary object controllers
+    test the implementation of the user_to_dict
     '''
 
     def test_simple_user_model_conversion(self):
         '''
-        create a simple user model and pass it to the content_to_dict controller
+        create a simple user model, pass to user_to_dict()
+        assert resulting dictionary is correct
         '''
-        sample_user = User(username='man', email='man@man.com', googleID='1').put()
+        sample_user = User(
+            username='man',
+            email='man@man.com',
+            googleID='1'
+            ).put()
         dict_user = user_to_dict(sample_user.get())
         self.assertEqual(dict_user['username'], 'man')
         self.assertEqual(dict_user['email'], 'man@man.com')
