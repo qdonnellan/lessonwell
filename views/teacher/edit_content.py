da744d96812d5a52ba9f934ad4509761adae3fac
qdonnellan <qdonnellan@gmail.com>
2014-03-16 17:57:55 -0500

    added an edit content view handler

---
+++ views/teacher/edit_content.py
@@ -0,0 +1,14 @@
+from views.main_view_handler import ViewHandler
+from decorators.auth import local_user_required
+
+class EditContent(ViewHandler):
+    '''
+    the request handler for the edit content page
+    '''
+    
+    @local_user_required
+    def get(self):
+        '''
+        handle the get request for the edit content page
+        '''
+        self.render('edit.html')
