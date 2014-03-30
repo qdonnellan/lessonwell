593ae2096f7869d642365f4264ab95e0b0a6f623
qdonnellan <qdonnellan@gmail.com>
2014-03-29 20:02:01 -0500

    updated test

--- tests/api_tests/api_controllers_content_to_dict_test.py
+++ tests/api_tests/api_controllers_content_to_dict_test.py
@@ -1,19 +1,27 @@
 from tests.main_test_handler import TestBase
-from models.content import Content
+from controllers.new_course import new_course
+from controllers.fetch_curriculum import get_content_by_id
 from models.user import User
+
 from api.api_controllers.content_to_dict import content_to_dict
 
 class ContentToDictTest(TestBase):
-    '''
-    test the implementation of the content model to dictionary object controllers
-    '''
+    """
+    test the implementation of the content model to 
+    dictionary object controllers
+    """
 
     def test_simple_content_model_conversion(self):
-        '''
+        """
         create a simple model and pass it to the content_to_dict controller
-        '''
-        sample_user = User(username='man', email='man@man.com', googleID='1').put()
-        sample_content = Content(title='foo', body='bar', contentType='contentType', parent=sample_user).put()
-        dict_content = content_to_dict(sample_content.get())
-        self.assertEqual(dict_content['title'], 'foo')
-        self.assertIsNone(dict_content['listed'])
+        """
+        local_user = self.create_and_return_local_user()
+        course_id = new_course({
+            'teacher' : local_user.key.id(),
+            'title' : 'foo course',
+            'body' : 'hey look mom',
+            })
+        course = get_content_by_id(course_id)
+        dict_content = content_to_dict(course)
+        self.assertEqual(dict_content['content_type'], 'course')
+        self.assertEqual(dict_content['content']['title'], 'foo course')
