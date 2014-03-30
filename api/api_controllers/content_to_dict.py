9cb1a84bb6f77e75e7613abf258c74be88c73b19
qdonnellan <qdonnellan@gmail.com>
2014-03-29 19:55:29 -0500

    simplified the content_to_dict controller, some more modifications of this are coming...

--- api/api_controllers/content_to_dict.py
+++ api/api_controllers/content_to_dict.py
@@ -1,20 +1,11 @@
 def content_to_dict(content):
-    '''
-    return a dictionary object based on the content model passed
-    '''
+    """
+    return a dictionary object based on the curriculum model passed
+    """
 
     data = {}
-
-    data['content_type'] = content.contentType 
-    data['title'] = content.title
-    data['parent_id'] = content.key.parent().id()
+    data['content_type'] = content.content_type
     data['id'] = content.key.id()
-    data['body'] = content.body
-    data['active'] = content.active
-    data['last_modified'] = content.last_modified.strftime("%Y-%m-%d %H:%M:%S")
-    data['created'] = content.created.strftime("%Y-%m-%d %H:%M:%S")
-    data['listed'] = content.listed
-    data['privacy'] = content.privacy
-    data['passphrase'] = content.passphrase
+    data['content'] = content.content
 
     return data
