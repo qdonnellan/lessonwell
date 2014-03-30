30de135616ca9a9849c48b19cfb151443cc24df9
qdonnellan <qdonnellan@gmail.com>
2014-03-29 19:56:07 -0500

    DOCSTRING updates here

--- api/request_handlers/user_api.py
+++ api/request_handlers/user_api.py
@@ -3,14 +3,14 @@ from controllers.fetch_user import get_user
 from api.api_controllers.user_to_dict import user_to_dict
 
 class UserAPI(APIHandler):
-    '''
+    """
     handle all get/post requests for the user api
-    '''
+    """
 
     def get(self, userID):
-        ''' 
+        """
         handle the get request userID; return none if userID does not exist
-        '''
+        """
         try:
             user = get_user(userID)
             if not user:
