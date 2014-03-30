7753a4567f6eb989e33e48fd01ddebd4134e419d
qdonnellan <qdonnellan@gmail.com>
2014-03-29 20:07:12 -0500

    removed line here

--- models/curriculum.py
+++ models/curriculum.py
@@ -4,7 +4,6 @@ class Curriculum(ndb.Model):
     """
     the new curriculum flat curriculum model
     """
-
     content_type = ndb.StringProperty(required = True)
     content = ndb.JsonProperty(required=False)
 
