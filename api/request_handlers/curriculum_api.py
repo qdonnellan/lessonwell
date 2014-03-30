a70130722e2b46fdab3e6ebfc34f2472b23ae864
qdonnellan <qdonnellan@gmail.com>
2014-03-29 19:54:39 -0500

    renamed the content api

--- api/request_handlers/curriculum_api.py
+++ api/request_handlers/curriculum_api.py
@@ -1,20 +1,21 @@
 from main_api_handler import APIHandler
-from controllers.fetch_content_by_id import get_content
+from controllers.fetch_curriculum import get_content_by_id
 from api.api_controllers.content_to_dict import content_to_dict
 from decorators.auth import check_approval
 
-class ContentAPI(APIHandler):
-    '''
+class CurriculumAPI(APIHandler):
+    """
     handle all get/post requests for content type things
-    '''
+    """
 
     @check_approval
-    def get(self, userID=None, courseID=None, unitID=None, lessonID=None):
-        ''' 
-        handle the get request for the content api, exceptions are caught and passed in the 'error' param
-        '''
+    def get(self, contentID):
+        """
+        handle the get request for the content api, 
+        exceptions are caught and passed in the 'error' param
+        """
         try:
-            content = get_content(userID, courseID, unitID, lessonID)
+            content = get_content_by_id(contentID)
             data = content_to_dict(content)
         except Exception as e:
             data = {'error' : str(e)}
