bf89ecf9e773e81e900aa9cd4d3b338078d47042
qdonnellan <qdonnellan@gmail.com>
2014-03-29 19:55:58 -0500

    DOCSTRING updates here

--- api/request_handlers/main_api_handler.py
+++ api/request_handlers/main_api_handler.py
@@ -2,13 +2,14 @@ import webapp2
 import json
 
 class APIHandler(webapp2.RequestHandler):
-    '''
+    """
     API Handler inherited by all api request handlers to make things easier
-    '''
+    """
 
     def write_json(self, data):
-        '''
-        take the passed data, add JSON headers, and encode the data as a json string
-        '''
+        """
+        take the passed data, add JSON headers, 
+        and encode the data as a json string
+        """
         self.response.headers['Content-Type'] = 'application/json' 
         self.response.out.write(json.dumps(data))
