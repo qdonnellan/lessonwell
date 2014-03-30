15707fe9882f282d499b10d8398a02b5ee044c44
qdonnellan <qdonnellan@gmail.com>
2014-03-29 20:05:09 -0500

    new approval method reflected in these tests

--- tests/controller_tests/modify_approval_test.py
+++ tests/controller_tests/modify_approval_test.py
@@ -1,51 +1,38 @@
 from tests.main_test_handler import TestBase
 from controllers.modify_approval import new_approval_request, update_approval
-from models.approval import Approval
-from models.content import Content
-from google.appengine.ext import ndb
+from models.curriculum import Curriculum 
+from controllers.fetch_curriculum import get_course_by_id
+from controllers.new_course import new_course
 
 class ModifyApprovalTest(TestBase):
-    '''
-    test the implementation of the controllers which create and modify approval
-    '''
-    def create_and_return_new_approval(self):
-        '''
+    """
+    test the implementation of the controllers which create 
+    and modify approval
+    """
+    def create_new_approval(self):
+        """
         a helper class for quickly creating a new approval item
-        '''
-        user = self.create_and_return_local_user()
-        course_id = self.create_sample_course(user=user)
-        course = ndb.Key(Content, int(course_id), parent=user.key).get()
-        new_approval_request(course, googleID='123', formalName='John Doe', email='j@example.com')
-        return Approval.query().get()
+        """
+        teacher = self.create_and_return_local_user()
+        course_id = new_course({
+            'teacher' : teacher.key.id(),
+            'title' : 'foo course',
+            'body' : 'hey look mom',
+            })
+        new_approval_request(
+            course = get_course_by_id(course_id), 
+            googleID='123', 
+            formalName='John Doe', 
+            email='j@example.com')
 
     def test_new_approval_request(self):
-        '''
+        """
         create a new approval and assert that it shows up in the database
-        '''
-        approval_instance = self.create_and_return_new_approval()
-        self.assertEqual(approval_instance.googleID, '123')
-        self.assertEqual(approval_instance.status, 'pending')
+        """
+        self.create_new_approval()
+        course = Curriculum.query().get()
+        self.assertIn('123', course.content['pending_approval'])
 
-    def test_update_approval_request(self):
-        '''
-        create a new approval then edit it with the update_approval request
-
-        changes should be reflected in the database
-        '''
-        old_approval = self.create_and_return_new_approval()
-        course = old_approval.key.parent().get()
-        update_approval(course = course, googleID = '123', status='approved')
-        updated_approval = Approval.query().get()
-        self.assertEqual(updated_approval.status, 'approved')
-
-    def test_delete_approval_request(self):
-        '''
-        a deleted approval should be deleted from the database
-        '''
-        approval_instance = self.create_and_return_new_approval()
-        course = approval_instance.key.parent().get()
-        update_approval(course = course, googleID = '123', status='delete')
-        self.assertEqual(Approval.query().count(), 0)
 
 
 
