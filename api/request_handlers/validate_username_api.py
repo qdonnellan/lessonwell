cb68850ece8bcdbacd660f1ffbe163e1a74ef733
qdonnellan <qdonnellan@gmail.com>
2014-03-29 19:56:16 -0500

    DOCSTRING updates here

--- api/request_handlers/validate_username_api.py
+++ api/request_handlers/validate_username_api.py
@@ -2,14 +2,14 @@ from main_api_handler import APIHandler
 from controllers.validation import validate_username
 
 class ValidateUsernameAPI(APIHandler):
-    '''
+    """
     handle the requests for a valid_username
-    '''
+    """
 
     def get(self, username):
-        ''' 
+        """
         return a json string with an 'error' param if username is bad or a 'success' param if valid
-        '''
+        """
         try:
             validate_username(username)
         except Exception as e:
