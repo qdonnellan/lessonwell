22aef66d885840936738302ace48ff1b53a391dd
qdonnellan <qdonnellan@gmail.com>
2014-04-02 17:07:30 -0500

    some tests for the user api

--- tests/api_tests/api_user_test.py
+++ tests/api_tests/api_user_test.py
@@ -1,4 +1,5 @@
 from tests.main_test_handler import TestBase
+from controllers.new_course import new_course
 import json
 
 class GetUserAPITest(TestBase):
@@ -23,6 +24,39 @@ class GetUserAPITest(TestBase):
         response = self.testapp.get('/api/user/9999999')
         json_user = json.loads(response.body)
         self.assertEqual(json_user['error'], 'that user does not exist')
+
+    def test_user_with_courses(self):
+        """
+        create a user + a course and see that the course
+        is actually appended to the user's json object
+        """
+        teacher = self.create_and_return_local_user(username='foouser')
+        user_id = teacher.key.id()
+        content = {
+            'teacher' : int(teacher.key.id()),
+            'title' : 'foo course',
+            'body' : 'study hard, learn well, duh',
+        }
+        course_id = new_course(content)
+        response = self.testapp.get('/api/user/%s' % user_id)
+        user_data = json.loads(response.body)
+        user_courses = user_data['courses']
+        self.assertIn(str(course_id), user_courses)
+
+    def test_fetch_current_user(self):
+        """
+        make a call to '/api/user' with no id specified the get
+        implies a request for the current user
+        """
+        self.create_google_user(user_id = '123')
+        teacher = self.create_and_return_local_user(googleID = '123')
+        teacher_id = teacher.key.id()
+        response = self.testapp.get('/api/user')
+        user_data = json.loads(response.body)
+        self.assertNotIn('error', user_data)
+        self.assertEqual(user_data['id'], teacher_id)
+
+
 
         
 
