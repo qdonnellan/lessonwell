8d51b40e0dfc73facc5dea49f702cee71a63b422
qdonnellan <qdonnellan@gmail.com>
2014-04-02 17:04:36 -0500

    updated imports

--- api/request_handlers/curriculum_api.py
+++ api/request_handlers/curriculum_api.py
@@ -1,7 +1,12 @@
 from main_api_handler import APIHandler
 from controllers.fetch_curriculum import get_content_by_id
 from api.api_controllers.content_to_dict import content_to_dict
-from decorators.auth import check_approval
+from decorators.auth import check_approval, local_user_required
+from google.appengine.api import users
+from controllers.fetch_user import get_user_by_google_id
+from controllers.new_course import new_course
+from controllers.new_unit import new_unit
+import logging
 
 class CurriculumAPI(APIHandler):
     """
