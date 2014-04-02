4c7f48db8a4e9de93f8a6b40cb7b89cff3d28323
qdonnellan <qdonnellan@gmail.com>
2014-04-02 17:03:04 -0500

    user to dictionary function updated

--- api/api_controllers/user_to_dict.py
+++ api/api_controllers/user_to_dict.py
@@ -1,8 +1,10 @@
+from controllers.fetch_curriculum import get_course_by_id
+from content_to_dict import content_to_dict
+
 def user_to_dict(user):
     """
     return a dictionary object based on the user model passed
     """
-
     data = {}
 
     data['username'] = user.username
@@ -13,5 +15,10 @@ def user_to_dict(user):
     data['email'] = user.email
     data['bio'] = user.bio
     data['stripeID'] = user.stripeID
+    data['courses'] = []
+    if user.courses:
+        for course_id in user.courses:
+            course = get_course_by_id(course_id)
+            data['courses'].append(content_to_dict(course))
 
     return data
