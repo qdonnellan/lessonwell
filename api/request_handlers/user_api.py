a0f5959c35bb999c1ea477785e8faa133d671536
qdonnellan <qdonnellan@gmail.com>
2014-04-02 17:05:22 -0500

    updates to the user api

--- api/request_handlers/user_api.py
+++ api/request_handlers/user_api.py
@@ -1,18 +1,23 @@
 from main_api_handler import APIHandler
-from controllers.fetch_user import get_user
+from controllers.fetch_user import get_user, get_user_by_google_id
 from api.api_controllers.user_to_dict import user_to_dict
+from google.appengine.api import users
 
 class UserAPI(APIHandler):
     """
     handle all get/post requests for the user api
     """
 
-    def get(self, userID):
+    def get(self, userID=None):
         """
         handle the get request userID; return none if userID does not exist
         """
         try:
-            user = get_user(userID)
+            if userID is None:
+                googleID = users.get_current_user().user_id()
+                user = get_user_by_google_id(googleID)
+            else:
+                user = get_user(userID)
             if not user:
                 raise Exception('that user does not exist')
             data = user_to_dict(user)
