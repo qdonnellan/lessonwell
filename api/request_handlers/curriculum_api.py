4f54425e7c7d2eace989640de5f642a579a1f028
qdonnellan <qdonnellan@gmail.com>
2014-04-02 17:04:23 -0500

    some modifications to the way the curriculum content is delivered to a get request

--- api/request_handlers/curriculum_api.py
+++ api/request_handlers/curriculum_api.py
@@ -11,13 +11,20 @@ class CurriculumAPI(APIHandler):
     @check_approval
     def get(self, contentID):
         """
-        handle the get request for the content api, 
+        handle the get request for the CurriculumAPI, 
         exceptions are caught and passed in the 'error' param
         """
         try:
             content = get_content_by_id(contentID)
             data = content_to_dict(content)
+            if content.content_type == 'course':
+                unit_list = []
+                for unit_id in content.content['units']:
+                    unit = get_content_by_id(unit_id)
+                    unit_list.append(content_to_dict(unit))
+                data['units'] = unit_list
         except Exception as e:
             data = {'error' : str(e)}
 
         self.write_json(data)
+
