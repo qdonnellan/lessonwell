25f4190905e86ec0a42a37c376c7d49c11edeed8
qdonnellan <qdonnellan@gmail.com>
2014-05-03 11:38:25 -0500

    added a controller to fetch the user's card

---
+++ controllers/stripe_controllers/get_user_card.py
@@ -0,0 +1,24 @@
+from google.appengine.api import users
+from controllers.fetch_user import get_user_by_google_id
+from stripe_keys import api_key
+import stripe
+
+def get_user_card():
+    """
+    return card information for the current user
+    """
+
+    stripe.api_key = api_key
+
+    google_user = users.get_current_user()
+    googleID = google_user.user_id()
+    local_user = get_user_by_google_id(googleID)
+
+    stripeID = local_user.stripeID
+    customer = stripe.Customer.retrieve(stripeID)
+    # return the first card from the customer's card list
+    return customer.cards.data[0]
+
+
+
+
