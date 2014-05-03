5569d286c500f39403aae8c833485a5ae4eb949e
qdonnellan <qdonnellan@gmail.com>
2014-05-03 11:37:51 -0500

    api handler for the user card

---
+++ api/request_handlers/user_card_api.py
@@ -0,0 +1,32 @@
+from main_api_handler import APIHandler
+from controllers.stripe_controllers.get_user_card import get_user_card
+from controllers.stripe_controllers.change_card import change_card
+from decorators.auth import local_user_required
+import logging
+
+class UserCardAPI(APIHandler):
+    """
+    handle all get/post requests for the user_card api
+    """
+
+    @local_user_required
+    def get(self):
+        """
+        handle the get request for the user_card api
+        """
+        try:
+            data = get_user_card()
+
+        except Exception as e:
+            data = {'error' : str(e)}
+
+        self.write_json(data)
+
+    @local_user_required
+    def post(self):
+        """
+        change the card on file
+        """
+        card_info = self.request.POST.get('stripeToken')
+        change_card(card_info)
+        self.redirect('/edit')
