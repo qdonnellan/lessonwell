31d8299aa46f76504e46a0eb4e9a4155b31ce109
qdonnellan <qdonnellan@gmail.com>
2014-05-03 11:37:31 -0500

    new controller for modifying the curriculum

---
+++ controllers/modify_curriculum.py
@@ -0,0 +1,18 @@
+from fetch_user import get_user
+from models.curriculum import Curriculum
+from controllers.fetch_curriculum import get_content_by_id
+
+def modify_content(modified_content, content_id):
+    """
+    edit existing content, return the id of that content object
+    """
+    content_object = get_content_by_id(content_id)
+    if 'title' in modified_content:
+        content_object.content['title'] = modified_content['title']
+
+    if 'body' in modified_content:
+        content_object.content['body'] = modified_content['body']
+
+    content_object.put()
+    return content_id
+    
